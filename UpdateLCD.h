dc3e0d6a975dca7f117bcfd5455931840ad19bcf
Aleksei Tsikin <aleksei.tsikin@tll.aero>
2020-07-24 22:25:51 +0300

    Removed Classes

--- UpdateLCD.h
+++ UpdateLCD.h
@@ -1,85 +1,34 @@
-#include <LiquidCrystal_I2C.h>
 #include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, A4, A5);
-
-typedef enum {
-  HOMEPAGE,
-  SETUPPAGE
-} page;
+#define ROWS 2
+#define COLUMNS 16
+#define ADDRESS 0x27
 
 
-class UpdateLCD {
-  public:
-    UpdateLCD();
-    void print(uint8_t, uint8_t, String);
-    void print(uint8_t, uint8_t, int);
-    void print(uint8_t, uint8_t, float);
-    void printPage(page);
+LiquidCrystal_I2C lcd(ADDRESS, COLUMNS, ROWS); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
-    void setCurrentPage(page);
-    page getCurrentPage();
-
-  private:
-    ControlMode mode;
-    page currentPage;
-    void printHomePage();     //for stacic texts
-    void printSetupPage();    //for stacic texts
-    String cntrlModeString[6] = {"Max temp", "Graph", "LH", "Nurk", "Koef", "Hour"};
-};
-
-UpdateLCD::UpdateLCD() {
+void initLcd() {
+  lcd.init();
   lcd.init();
   lcd.backlight();
-  lcd.clear();
-  printPage(HOMEPAGE);
-  currentPage = HOMEPAGE;
 }
 
-void UpdateLCD::print (uint8_t column, uint8_t row, String text) {
-  lcd.setCursor(column, row);
-  lcd.print(text);
-}
-
-void UpdateLCD::print (uint8_t column, uint8_t row, int value) {
-  lcd.setCursor(column, row);
+template <typename T>
+void print(uint8_t columns, uint8_t rows, T value) {
+  lcd.setCursor(columns, rows);
   lcd.print(value);
 }
 
-void UpdateLCD::print (uint8_t column, uint8_t row, float value) {
-  lcd.setCursor(column, row);
-  lcd.print(value);
+void printHomePage() {
+  const String homePageStrings[] = {"Mode:", "kW", "Set t:", "t:"};
+  const uint8_t cursorColumnPos[] = {0, 14, 0, 11};
+  const uint8_t cursorRowPos[] = {0, 0, 1, 1};
+  for (int i = 0; i < 4; i++)
+    print(cursorColumnPos[i], cursorRowPos[i], homePageStrings[i]);
 }
 
-void UpdateLCD::printPage(page curretnPage) {
-  switch (curretnPage) {
-    case HOMEPAGE:
-      printHomePage();
-      break;
-    case SETUPPAGE:
-      printSetupPage();
-      break;
-    default: return;
-  }
-}
-
-void UpdateLCD::printHomePage() {
-  //print all static text for home page here
-  const String ctrlMode  = "Control mode: ";
-  print(0, 1, ctrlMode);
-  print(0, 1, cntrlModeString[mode.getControlType()]);
-}
-
-void UpdateLCD::printSetupPage() {
-  //print all static text for setup page here
-  const String chngCntrlMode = "Change mode: ";
-  print(0, 1, chngCntrlMode);
-}
-
-void UpdateLCD::setCurrentPage(page pagex) {
-  currentPage = pagex;
-}
-
-page UpdateLCD::getCurrentPage() {
-  return currentPage;
+void printSetupPage() {
+  const String chngCntrlMode = "Change mode.";
+  print(0, 0, chngCntrlMode);
 }
