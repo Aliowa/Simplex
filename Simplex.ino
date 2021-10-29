165fb1cd19fb21ba9f2d5952445517d67a95a6fb
Aleksei Tsikin <aleksei.tsikin@ericsson.com>
2021-10-29 09:54:27 +0300

    Removed old stuff
    To do, adjust rotation im ms
    Add inteerupts on buttons
    Remove set_temp and current_temp t from structures, refere to data strored i memmory map.

--- Simplex.ino
+++ Simplex.ino
@@ -1,181 +1,268 @@
-#include "enums.h"
-#include "UpdateLCD.h"
-#include "ControlMode.h"
-#include <Wire.h>
-#include <DallasTemperature.h>
+/*
+   Set pot to around 2047 using following:
+  void setup() {
+    Serial.begin(115200);
+  }
+  void loop() {
+    Serial.println("Pot value: "+ String(analogRead(26)));
+    delay(100);
+  }
 
-#define UP 16
-#define DOWN 19
-#define MENU 5
+  Then programm unit once and comment out #define FORCE_ZERO_POINT*
+*/
 
-// Data wire is plugged into port 2 on the Arduino
-#define SENSOR_0 25
-#define SENSOR_1 32
-#define SENSOR_2 33
+//i2c required pins
+#define I2C_SDA 23
+#define I2C_SCL 22
 
-#define MOTOR_PIN_1 35
-#define MOTOR_PIN_2 34
-#define MOTOR_PIN_3 39
-#define MOTOR_PIN_4 36
+//LED predefine pins
+#define LED_R_A 4
+#define LED_R_B 17
+#define LED_B_A 18
+#define LED_B_B 21
 
-#define LED_R_1 4
-#define LED_R_2 17
-#define LED_B_1 18
-#define LED_B_2 21
+//Button predefined pins
+#define BUTTON_UP 16
+#define BUTTON_DOWN 19
+#define BUTTON_MENU 5
 
+//Potentiometer predefined pin
 #define POT_PIN 26
 
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-OneWire sensor_0(SENSOR_0);
-OneWire sensor_1(SENSOR_1);
-OneWire sensor_2(SENSOR_2);
+//Motor pins
+#define MOTOR_PIN_A 32
+#define MOTOR_PIN_B 33
+#define MOTOR_CW digitalWrite(MOTOR_PIN_A, HIGH); digitalWrite(MOTOR_PIN_B, LOW);
+#define MOTOR_CCW digitalWrite(MOTOR_PIN_A, LOW); digitalWrite(MOTOR_PIN_B, HIGH);
+#define MOTOR_STOP digitalWrite(MOTOR_PIN_A, LOW); digitalWrite(MOTOR_PIN_B, LOW);
 
-// Pass our oneWire reference to Dallas Temperature.
-DallasTemperature sens0(&sensor_0);
-DallasTemperature sens1(&sensor_1);
-DallasTemperature sens2(&sensor_2);
+//Temeperature pins
+#define TEMP_PIN 25
 
+#include "I2CEEPROM.h"
+#include <OneWire.h>
+#include <Wire.h>
+#include <DallasTemperature.h>
+#include "LiquidCrystal_I2C.h"
+
+OneWire one_wire_pin(TEMP_PIN);
+DallasTemperature sensA(&one_wire_pin);
+DallasTemperature sensB(&one_wire_pin);
+DallasTemperature sensC(&one_wire_pin);
 DallasTemperature* sensors[3];
 float temp[3];
 
-uint64_t readSensTime;
-uint16_t readInterval = 10000; //read sensors with 10s interval
-int currentSetupPage;
+LiquidCrystal_I2C lcd(0x20, 16, 2); // set the LCD address to 0x20 for a 16 chars and 2 line display
+I2CEEPROM eep(0x50);
 
+//Home page static prints
+const struct {
+  uint8_t column;
+  uint8_t row;
+  String data;
+} home_page[] = {
+  {0, 0, "Mode:"},
+  {14, 0, "kW"},
+  {0, 1, "Set t:"},
+  {10, 1, "t:"}
+};
 
+//Setup page static prints
+const struct {
+  uint8_t column;
+  uint8_t row;
+  String data;
+} setup_page[] = {
+  {0, 0, "Change Mode:"},
+  {0, 0, "LCD Contrast:"},
+  {0, 0, "Firmware:"}
+};
+
+//Home page kW calc
+struct {
+  const uint8_t colum = 11;
+  const uint8_t row = 0;
+  float kW = 0;
+} kW;
+
+//Home page current temp position and data
+struct {
+  const uint8_t column = 12;
+  const uint8_t row = 1;
+  float t = 0;
+} current_temp;
+
+//Home page set temperature position and data
+struct {
+  const uint8_t column = 6;
+  const uint8_t row = 1;
+  float t = 0;
+} set_temp;
+
+//EEPROM map: 20 - Set temerature; 21 - Potentiometer zero point value; 22 - Potentiomter zero point value set
+struct {
+  const uint8_t addres;
+  uint8_t data;
+} memmory_map[] = {
+  {20, 0},
+  {21, 0},
+  {22, 0}
+};
+
+const struct {
+  uint8_t min;
+  uint8_t max;
+} zone[] = {
+  {0, 63},
+  {64, 127},
+  {128, 191},
+  {192, 255}
+};
+
+#define FORCE_ZERO_POINT
+
+//Set temperature min ad max
+const struct {
+  uint8_t min = 10;
+  uint8_t max = 85;
+} min_max_temp;
+
+//Sensor reading related data
+uint64_t read_sensors_time;
+const uint16_t read_sensors_interval = 10000;
+
+//Pot inital value
+uint16_t pot_zero_value;
+
+//Initial setup
 void setup() {
   Serial.begin(115200);
 
-  initLcd();
-  printHomePage();
-  getInitialData();
+  Wire.begin(I2C_SDA, I2C_SCL);
+  lcd.init();
+  lcd.backlight();
 
-  sensors[0] = &sens0;
-  sensors[1] = &sens1;
-  sensors[2] = &sens2;
+  sensors[0] = &sensA;
+  sensors[1] = &sensB;
+  sensors[2] = &sensC;
 
-  pinMode(UP, INPUT);
-  pinMode(DOWN, INPUT);
-  pinMode(MENU, INPUT);
-  pinMode(MOTOR_PIN_1, OUTPUT);
-  pinMode(MOTOR_PIN_2, OUTPUT);
-  pinMode(MOTOR_PIN_3, OUTPUT);
-  pinMode(MOTOR_PIN_4, OUTPUT);
-  pinMode(LED_R_1, OUTPUT);
-  pinMode(LED_R_2, OUTPUT);
-  pinMode(LED_B_1, OUTPUT);
-  pinMode(LED_B_2, OUTPUT);
+  for (int sensor = 0; sensor < 3; sensor++)
+    sensors[sensor]->begin();
 
-  currentPage = HOME;
+  pinMode(BUTTON_UP, INPUT);
+  pinMode(BUTTON_DOWN, INPUT);
+  pinMode(BUTTON_MENU, INPUT);
+  pinMode(MOTOR_PIN_A, OUTPUT);
+  pinMode(MOTOR_PIN_B, OUTPUT);
+  pinMode(LED_R_A, OUTPUT);
+  pinMode(LED_R_B, OUTPUT);
+  pinMode(LED_B_A, OUTPUT);
+  pinMode(LED_B_B, OUTPUT);
 
-  for (int i = 0; i < 3; i++)
-    sensors[i]->begin();
+  MOTOR_STOP
 
-  readSensors();
-  readSensTime = millis();
-  print(12, 1, temp[0]);
+  //Print Home page static data
+  for (int i = 0; i < 4; i++)
+    lcd_print(home_page[i].column, home_page[i].row, home_page[i].data);
 
-  currentSetupPage = 0;
+
+  //Read and print initial data
+  read_sensors();
+  lcd_print(current_temp.column, current_temp.row, current_temp.t);
+  memmory_map[0].data = eep.read(memmory_map[0].addres);
+  set_temp.t = memmory_map[0].data;
+  lcd_print(set_temp.column, set_temp.row, memmory_map[0].data);
+
+#ifdef FORCE_ZERO_POINT
+  eep.write(memmory_map[1].addres, byte(map(analogRead(POT_PIN), 0, 4095, 0, 255)));
+  eep.write(memmory_map[2].addres, 1);
+#endif
 }
+
+//Move button to interrupts
 
 void loop() {
-  //read sensors if @ HOME page with 10s interavl
-  if (millis() - readSensTime > readInterval && currentPage == HOME) {
-    readSensors();
-    readSensTime = millis();
-    print(12, 1, temp[0]);
+  //Read sensors each {read_sensors_interval}
+  if (millis() - read_sensors_time > read_sensors_interval) {
+    read_sensors();
+    lcd_print(current_temp.column, current_temp.row, current_temp.t);
+    read_sensors_time = millis();
+    update_temp();
   }
 
-  //read + button
-  if (digitalRead(UP) == HIGH)
-    upButton();
+  //Check if UP button is pressed
+  if (digitalRead(BUTTON_UP))
+    button_up();
 
-  //read M button
-  if (digitalRead(MENU) == HIGH)
-    menuButton();
+  //Check if DOWN button is pressed
+  if (digitalRead(BUTTON_DOWN))
+    button_down();
 
-  //read - button
-  if (digitalRead(DOWN) == HIGH)
-    downButton();
+  //Check if MENU button is pressed
+  if (digitalRead(BUTTON_MENU))
+    button_menu();
 
-  //Exit SETUP page if inactive for 10s
-  if (millis() - lastSetupAction > 10000 && setupEnd == false) {
-    currentPage = HOME;
-    printHomePage();
-    readSensors();
-    print(12, 1, temp[0]);
-    setupEnd = true;
-  }
 }
 
-void upButton() {
-  switch (currentPage) {
-    case HOME:
-      break;
-    case SETUP:
-      currentSetupPage++;
-      if (currentSetupPage > 2)
-        currentSetupPage = 0;
-      printSetupPage(&currentSetupPage);
-      break;
-    default: return;
-  }
+template <typename T>
+void lcd_print(uint8_t column, uint8_t row, T value) {
+  lcd.setCursor(column, row);
+  lcd.print(value);
 }
 
-void menuButton() {
-  switch (currentPage) {
-    case HOME:
-      currentPage = SETUP;
-      printSetupPage(&currentSetupPage);
-      break;
-    case SETUP:
-      currentPage = SETUP_EXTRA;
-      printHomePage();
-      readSensors();
-      print(12, 1, temp[0]);
-      break;
-    case SETUP_EXTRA:
-      break;
-    default: return;
-  }
-  delay(200); //kinda debounce
-}
-
-void downButton() {
-  switch (currentPage) {
-    case HOME: break;
-    case SETUP:
-      currentSetupPage--;
-      if (currentSetupPage < 0)
-        currentSetupPage = 2;
-      printSetupPage(&currentSetupPage);
-      break;
-    default: return;
-  }
-}
-
-void readSensors() {
+void read_sensors() {
   for (int i = 0; i < 3; i++) {
     sensors[i]->requestTemperatures();
     temp[i] = sensors[i]->getTempCByIndex(0);
   }
+  current_temp.t = temp[0];
 }
 
-void compareData(int * setTemp, int * sensorData) {
-  /*
-     compare data
-     if required, call update 3Dvent
-  */
+void update_temp() {
+  float dif_temp = set_temp.t - current_temp.t;
+  if (abs(dif_temp) > 0.1) {
+    if (dif_temp > 0)
+      rotate_motor(1, dif_temp);  //update to 0 if roattion should be oposite
+    else
+      rotate_motor(0, dif_temp);  //update to 1 if roattion should be oposite
+  }
 }
 
-void update3DVent() {
-  /*
-     open/close valve
-  */
+void rotate_motor(int direction, float dif_temp) {
+  uint16_t rotation_delay = abs(dif_temp) * 1000;
+  rotation_delay = constrain(rotation_delay, 100, 1000); // rotatio max and min time
+  Serial.println(rotation_delay);
+  if (direction) {
+    MOTOR_CW  //rotate clockwise
+    delay(100);
+    MOTOR_STOP
+  } else {
+    MOTOR_CCW //rotate counter clockwise
+    delay(100);
+    MOTOR_STOP
+  }
 }
 
-void getInitialData() {
-  /*
-     read EEPROM at starupt
-  */
+
+void button_up() {
+  memmory_map[0].data++;
+  memmory_map[0].data = constrain(memmory_map[0].data, min_max_temp.min, min_max_temp.max);
+  eep.write(memmory_map[0].addres, memmory_map[0].data);
+  set_temp.t = memmory_map[0].data;
+  lcd_print(set_temp.column, set_temp.row, eep.read(memmory_map[0].addres));
+  delay(200); //Kind of debounce
+}
+
+void button_down() {
+  memmory_map[0].data--;
+  memmory_map[0].data = constrain(memmory_map[0].data, min_max_temp.min, min_max_temp.max);
+  eep.write(memmory_map[0].addres, memmory_map[0].data);
+  set_temp.t = memmory_map[0].data;
+  lcd_print(set_temp.column, set_temp.row, eep.read(memmory_map[0].addres));
+  delay(200); //Kind of debounce
+}
+
+
+void button_menu() {
+
 }
